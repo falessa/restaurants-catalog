4cfd145782f9da514322d75f02d9a4e6ed887790
Flavia Gramajo <flaviagramajo@gmail.com>
2023-05-30 21:45:16 +0200

    remove unnecessary code from Search.tsx

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import { Box, ThemeProvider } from '@mui/material'
 import { myTheme } from './theme/myTheme';
-import { useTranslation } from 'react-i18next';
-import './config/i18n/i18n'
 
 function Search() {
-  const { t } = useTranslation();
 
-  const styles = {
-    mainSearchBox: {
-      position: 'absolute',
-      top: '40%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      textAlign: 'center',
-      width: '100%'
-    }
-  }
   return (
     <ThemeProvider theme={myTheme}>
         <Box>
