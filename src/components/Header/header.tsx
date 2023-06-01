42d34ed281c43fc74ab5bc0caae4506717a744f8
Flavia Gramajo <flaviagramajo@gmail.com>
2023-06-01 21:28:09 +0200

    add some styling to the header: change logo, logo on the left, language selector on the right. Add some data-test

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,27 +1,42 @@
 import React, { FC, ReactElement } from 'react';
-import avocadoHeart from '../../resources/images/avocado_heart.png';
-
+import yelpLogo from '../../resources/images/yelp_logo.png';
 import { Box } from '@mui/material';
 import { Image } from 'mui-image';
 import { LanguageSelector } from '../LanguageSelector/languageSelector';
+import { useNavigate } from 'react-router-dom';
 
 export const Header: FC = (): ReactElement => {
-    return(
-        <Box 
-            display='flex'
-            flexDirection='row'
-            alignItems='flex-start'
-            mt={2}
-            padding={2}
-        >
-            <Image 
-                src={avocadoHeart}
-                height='100px'
-                width='100px'
-                alt='restaurant-catalog-logo'
-            />
-            <LanguageSelector />
+    const navigate = useNavigate();
+    
+    const styles = {
+        headerBox: {
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: "15px",
+            padding: "2px"
+        }
+    }
 
+    return(
+        <Box
+            data-test="app-header"
+            sx={styles.headerBox} 
+        >
+            <Box
+                data-test="app-logo"
+                onClick={() => navigate("/")}
+            >
+                <Image
+                src={yelpLogo}
+                height='50px'
+                width='140px'
+                alt='restaurant-catalog-logo'
+                />
+            </Box>
+            <Box>
+                <LanguageSelector />
+            </Box>
         </Box>
     )
 }
