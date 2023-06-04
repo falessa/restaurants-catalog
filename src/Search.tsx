585029022353144065f313c0ea118696763ddd94
Flavia Gramajo <flaviagramajo@gmail.com>
2023-06-04 22:57:14 +0200

    Display BusinessMainDetailsCard component in Search

--- src/Search.tsx
+++ src/Search.tsx
@@ -3,15 +3,20 @@ import { Box, ThemeProvider } from '@mui/material'
 import { myTheme } from './theme/myTheme';
 import { useTranslation } from 'react-i18next';
 import './config/i18n/i18n';
+import { BusinessMainDetailsCard } from './components/BusinessMainDetailsCard/businessMainDetailsCard';
 
 function Search() {
     const { t } = useTranslation();
+
+    const styles = {
+        businessDetailsCard: {
+
+        }
+    }
     
     return (
         <ThemeProvider theme={myTheme}>
-            <Box>
-                {t("main.title")}
-            </Box>
+            <BusinessMainDetailsCard />
         </ThemeProvider>
     );
 }
